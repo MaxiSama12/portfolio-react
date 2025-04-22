1f8a86403d18fafd770507653985b5452390508b
MaxiSama12 <maxiestebansama12@gmail.com>
2025-04-21 23:43:03 -0300

    Volvimos responsive el navegador

--- src/components/Navegador.jsx
+++ src/components/Navegador.jsx
@@ -4,15 +4,17 @@ import Navbar from 'react-bootstrap/Navbar';
 
 function BasicExample() {
   return (
-    <Navbar data-bs-theme="dark">
+    <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
       <Container>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Nav className=" centrador_texto">
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto text-center">
             <Nav.Link href="#sobre_Mi" className='link-nav'>SOBRE MÍ</Nav.Link>
             <Nav.Link href="#soft_Skills" className='link-nav'>HABILIDADES</Nav.Link>
             <Nav.Link href="#proyectos" className='link-nav'>PROYECTOS</Nav.Link>
             <Nav.Link href="#experiencia" className='link-nav'>EXPERIENCIA</Nav.Link>
           </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
