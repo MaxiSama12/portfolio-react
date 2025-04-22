9234c3ba82fe8b2cfecdfe47b90809e78938b0ec
MaxiSama12 <maxiestebansama12@gmail.com>
2025-04-22 00:30:19 -0300

    Hacemos responsive el navegador

--- src/components/Navegador.jsx
+++ src/components/Navegador.jsx
@@ -1,22 +1,28 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import '../styles/Navegador.css'
+import '../styles/Navegador.css';
 
 function BasicExample() {
   return (
-    <Navbar data-bs-theme="dark">
+    <Navbar collapseOnSelect expand="md"  variant="dark">
       <Container>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Nav className=" centrador_texto">
-            <Nav.Link href="#sobre_Mi" className='link-nav'>SOBRE MÍ</Nav.Link>
-            <Nav.Link href="#soft_Skills" className='link-nav'>HABILIDADES</Nav.Link>
-            <Nav.Link href="#proyectos" className='link-nav'>PROYECTOS</Nav.Link>
-            <Nav.Link href="#experiencia" className='link-nav'>EXPERIENCIA</Nav.Link>
+
+        {/* Botón de colapso alineado a la derecha */}
+        <Navbar.Toggle aria-controls="basic-navbar-nav" className="ms-auto" />
+
+        <Navbar.Collapse id="basic-navbar-nav" >
+          <Nav className="ms-auto nav-links">
+            <Nav.Link href="#sobre_Mi" className="link-nav">SOBRE MÍ</Nav.Link>
+            <Nav.Link href="#soft_Skills" className="link-nav">HABILIDADES</Nav.Link>
+            <Nav.Link href="#proyectos" className="link-nav">PROYECTOS</Nav.Link>
+            <Nav.Link href="#experiencia" className="link-nav">EXPERIENCIA</Nav.Link>
           </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
 }
 
 export default BasicExample;
+
