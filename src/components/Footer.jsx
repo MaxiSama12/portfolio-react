deac5280b3fb45f71d75a6130c451602446f73dd
MaxiSama12 <maxiestebansama12@gmail.com>
2025-04-21 23:15:01 -0300

    se agrego rel=noreferrer a los links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,9 @@ const Footer = () => {
   return (
     <footer>
     <div class="social-icons ">
-      <a href="https://github.com/MaxiSama12" target="_blank" title="Mi GitHub"><i class="fab fa-github"></i></a>
-      <a href="https://www.instagram.com/estebannn_______/" target="_blank"><i class="fab fa-instagram"></i></a>
-      <a href="https://www.linkedin.com/in/esteban-maximiliano-samaniego-33b4a3263/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+      <a href="https://github.com/MaxiSama12" target="_blank" rel="noreferrer" title="Mi GitHub"><i class="fab fa-github"></i></a>
+      <a href="https://www.instagram.com/estebannn_______/" target="_blank" rel="noreferrer"><i class="fab fa-instagram"></i></a>
+      <a href="https://www.linkedin.com/in/esteban-maximiliano-samaniego-33b4a3263/" target="_blank" rel="noreferrer"><i class="fab fa-linkedin-in"></i></a>
     </div>
   </footer>
   )
